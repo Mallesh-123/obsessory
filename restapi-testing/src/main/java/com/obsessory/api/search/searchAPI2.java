c2c147a27cde66f128bc9a4edfcd42f6f353c0fd
mallesh dollin <malleshd@kychub.com>
2020-12-23 12:42:22 +0000

    done

---
+++ restapi-testing/src/main/java/com/obsessory/api/search/searchAPI2.java
@@ -0,0 +1,32 @@
+package com.obsessory.api.search;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class searchAPI2 {
+
+    @Test
+    public void search() throws InterruptedException {
+
+        {
+            Response response = given()
+
+                    .header("Content-Type", "application/json")
+                    .when().get("https://api.obsessory.com/node/api/search?q=shoes&perPage=20&department=men&country=GB&internationaldelivery=GB&page=2")
+                    .then().extract().response();
+            int statusCode = response.statusCode();
+            System.out.println(response.asString());
+            System.out.println(statusCode);
+            Assert.assertEquals(statusCode, 200);
+            Object key = response.jsonPath().get("response.data.key");
+            System.out.println(key);
+            String expected = "Shoes for Men";
+            Assert.assertEquals(key,expected);
+
+
+        }
+    }
+}
---
+++ restapi-testing/src/main/java/com/obsessory/api/womens/womensAPI2.java
@@ -0,0 +1,32 @@
+package com.obsessory.api.womens;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class womensAPI2 {
+
+    @Test
+    public void search() throws InterruptedException {
+
+        {
+            Response response = given()
+
+                    .header("Content-Type", "application/json")
+                    .when().get("https://api.obsessory.com/node/api/search?link=/outerwear&perPage=20&internationaldelivery=GB&country=GB&page=2")
+                    .then().extract().response();
+            int statusCode = response.statusCode();
+            System.out.println(response.asString());
+            System.out.println(statusCode);
+            Assert.assertEquals(statusCode, 200);
+            Object key = response.jsonPath().get("response.data.key");
+            System.out.println(key);
+            String expected = "Outerwear for Women";
+            Assert.assertEquals(key,expected);
+
+
+        }
+    }
+}
